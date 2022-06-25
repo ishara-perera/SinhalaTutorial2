b39910d9434654105f55f3bf95d866d598940d32
Ishara Perera <ishara.perera@icloud.com>
2022-06-25 08:24:42 +0530

    Initial Commit

---
+++ SinhalaTutorial/Controllers/TasksController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SinhalaTutorial.Controllers;
+
+public class TasksController : Controller
+{
+    [HttpGet("tasks")]
+    public IActionResult Tasks()
+    {   
+        var tasks = new string[] { "Task1", "Task2", "Task3" };
+        return Ok(tasks);
+    }
+    
+    [HttpPost("create_task")]
+    public IActionResult CreateTask()
+    {
+        return Ok();
+    }
+}
